f7f4936d0354023f02d51cfaf0ef6b81a4d7ee1e
Vijayasekhar Deepak <Vijayasekhar.Deepak@walmart.com>
2021-04-28 18:27:13 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Mono repo
+    A React mono repo project.
+
+## Doc reference to build this repo
+    https://jibin.tech/monorepo-with-create-react-app/
---
+++ packages/app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ColorHeader from "@react-monorepo/common/components/ColorHeader";
+
+function App() {
+  return (
+    <div className="App">
+      <ColorHeader color="red">
+        App Header
+      </ColorHeader>
+    </div>
+  );
+}
+
+export default App;
